bad15d79c859ee7b346fdd92df750593047b0fbf
eddieUFSM <edrocha@inf.ufsm.br>
2021-01-29 10:56:56 -0300

    about page start setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/home.module.css'
-import Header from '../components/header/index'
+import styles from "@shared/home.module.css"
+import Header from "@components/header/index"
+import Navigation from '@components/Navigation/index'
 
 export default function Home() {
   return (
     <>
+
       <Head>
         <title> Create Next App</title>
-        <link rel="icon" href="favicon.ico" />
       </Head>
-
       <main className={styles.container}>
+        <Navigation />
         <Header />
       </main>
     </>
